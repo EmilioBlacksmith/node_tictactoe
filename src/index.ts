a20f81219212c783a20bb645bd6043e85b22e09b
Emilio Blacksmith <emilioblacksmithlush@gmail.com>
2024-02-16 18:47:20 -0600

    create user input module to handle inputs in the terminal

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+const { getUserInput, closeReadLine } = require("./userInput");
+
+let playerName: string = "";
+
+async function main(): Promise<void> {
+	playerName = await getUserInput("What's your name?\n");
+	console.log(`Hello, ${playerName}`);
+	closeReadLine();
+}
+
+main();
---
+++ src/userInput.ts
@@ -0,0 +1,31 @@
+const readline = require("node:readline");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+/**
+ * Prompts the user with a question and returns a promise that resolvers to the user's input.
+ * @param question question The question to prompt the user with.
+ * @returns A promise that resolves to the user's input.
+ */
+function getUserInput(question: string): Promise<string> {
+	return new Promise((resolve, reject) => {
+		rl.question(question, (input: string) => {
+			resolve(input);
+		});
+		rl.on("close", () => {
+			reject(new Error("Readline interface closed unexpectedly."));
+		});
+	});
+}
+
+/**
+ * Closes the readline interface.
+ */
+function closeReadLine() {
+	rl.close();
+}
+
+module.exports = { getUserInput, closeReadLine };
